abb0e043c0548af1f7b52b59d2290bc1f245ba32
Mocha-- <xibowangmelb@gmail.com>
2016-08-05 22:18:44 +1000

    init

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: './bundle',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: [
+                    'es2016',
+                    'es2015',
+                    'react'
+                ],
+                plugins: [
+                    'transform-class-properties'
+                ]
+            }
+        }, {
+            test: /\.styl$/,
+            loaders: [
+                'style-loader',
+                'css-loader?sourceMap',
+                'postcss-loader',
+                'stylus-loader'
+            ]
+        }]
+    },
+    devtool: 'source-map',
+    devServer: {
+        contentBase: './bundle'
+    }
+};
